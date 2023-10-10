000b806e95a07f6ea16f9f393f03086e42bfe1a9
FAGU <102469304+Fagu12@users.noreply.github.com>
2023-10-10 21:44:59 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -70,6 +70,30 @@ Function({
 	if (img.length == 0) {
 		img = ['https://i.imgur.com/qJUBCYm.jpeg']
 	}
+	const image = img[Math.floor(Math.random() * img.length)]
+	const type = image.endsWith('mp4') ? 'video' : 'image'
+	const buttonMessage = {
+		[type]: {
+			url: image
+		},
+		caption: `${msg}`,
+		footer: `${BOT_INFO.split(";")[0] || ' '}`,
+		buttons: [{
+				buttonId: prefix + 'ping',
+				buttonText: {
+					displayText: 'Speed Test'
+				},
+				type: 1
+			},
+			{
+				buttonId: prefix + 'list',
+				buttonText: {
+					displayText: 'List Commands'
+				},
+				type: 1
+			}
+		]
+	}
 	await message.send(msg);
 	/* var img = await parsedUrl(BOT_INFO)
 	if (img.length == 0) {
